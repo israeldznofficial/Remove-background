3364a2f158ffabc839a27f4301dae017b6c51df6
israel-dzn-official <ariellidany9@gmail.com>
2026-08-10 21:23:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const promoMsg = document.getElementById('promoMessage');
 
 // הגדרות API וקוד שדרוג
 const API_KEY = 'VS1Nj55zAtnN2eJhFx5NLGQk';
-const PROMO_CODE = 'BAR13G';
+const PROMO_CODE = 'NAVOG2';
 
 let selectedFile = null;
 let processedBlob = null;
